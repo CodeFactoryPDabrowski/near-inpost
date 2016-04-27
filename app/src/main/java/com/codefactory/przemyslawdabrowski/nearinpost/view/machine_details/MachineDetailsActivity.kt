6258d3a0e117c6c86371baeeb097ff506d5ca655
Przemyslaw <codefactorypdabrowski@gmail.com>
2016-04-27 18:23:14 +0200

    Add toolbar to MachineDetailsActivity.

--- app/src/main/java/com/codefactory/przemyslawdabrowski/nearinpost/view/machine_details/MachineDetailsActivity.kt
+++ app/src/main/java/com/codefactory/przemyslawdabrowski/nearinpost/view/machine_details/MachineDetailsActivity.kt
@@ -2,6 +2,10 @@ package com.codefactory.przemyslawdabrowski.nearinpost.view.machine_details
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.widget.ImageView
+import android.widget.TextView
+import butterknife.bindView
 import com.codefactory.przemyslawdabrowski.nearinpost.R
 import com.codefactory.przemyslawdabrowski.nearinpost.model.ui.MachineUi
 import com.codefactory.przemyslawdabrowski.nearinpost.view.base.BaseActivity
@@ -28,11 +32,25 @@ class MachineDetailsActivity : BaseActivity() {
         }
     }
 
+    val toolbar: Toolbar by bindView(R.id.machineDetailsToolbar)
+    val closeButton: ImageView by bindView(R.id.machineDetailsClose)
+    val inPostName: TextView by bindView(R.id.machineDetailsName)
     lateinit var bottomSlideLayout: BottomSlideLayout
+
+    /**
+     * Object to displays details.
+     */
+    lateinit var machineUi: MachineUi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.machine_details_activity)
+        var machine = intent.getParcelableExtra<MachineUi>(MACHINE_DETAILS_BUNDLE_KEY)
+        if (machine == null) {
+            throw IllegalArgumentException("Machine details cannot be null")
+        } else {
+            machineUi = machine
+        }
         initViews()
     }
 
@@ -40,6 +58,15 @@ class MachineDetailsActivity : BaseActivity() {
      * Initialize views.
      */
     private fun initViews() {
+        fun setUpToolbar() {
+            setSupportActionBar(toolbar)
+            val actionBar = supportActionBar
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(false)
+                actionBar.setDisplayShowTitleEnabled(false)
+            }
+        }
+        setUpToolbar()
         bottomSlideLayout = findViewById(R.id.machineDetailsSlideLayout) as BottomSlideLayout
         bottomSlideLayout.addListener(object : BottomSlideLayout.Listener {
             override fun onDragDismissed() {
@@ -50,6 +77,8 @@ class MachineDetailsActivity : BaseActivity() {
                 //Empty.
             }
         })
+        closeButton.setOnClickListener { view -> finish() }
+        inPostName.text = machineUi.name
     }
 
 
